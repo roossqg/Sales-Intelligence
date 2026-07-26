0edf8944494e6ed475db5fc1e93f7e3e552fa086
Neima <a01399122@gmail.com>
2026-07-26 11:34:42 -0300

    settings and database config-create

--- Scripts/database.py
+++ Scripts/database.py
@@ -1,10 +1,11 @@
-from sqlalchemy import create_engine,Column,Integer,Float,String,DateTime
+from sqlalchemy import create_engine,Column,Integer,Float,String,DateTime,func
 from sqlalchemy.orm import DeclarativeBase,MappedAsDataclass,Mapped,mapped_column,sessionmaker
 from datetime import datetime
+from typing import Optional
 
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+from settings import settings
 
 class Base(DeclarativeBase,MappedAsDataclass):
     pass
@@ -15,19 +16,30 @@ class sales(Base):
     __tablename__ = 'Sales_data'
 
     id : Mapped[int] = mapped_column(primary_key=True)
-    client_id : Mapped[int]
-    age : Mapped[int]
-    price : Mapped[float]
-    quantity : Mapped[int]
-    age_range : Mapped[str]
-    category : Mapped[str]
-    datetime : Mapped[datetime]
+    client_id : Mapped[Optional[int]] = mapped_column(nullable=True) 
+    age : Mapped[Optional[int]] = mapped_column(nullable=True) 
+    price : Mapped[Optional[float]] = mapped_column(nullable=True) 
+    quantity : Mapped[Optional[int]] = mapped_column(nullable=True) 
+    age_range : Mapped[Optional[str]] = mapped_column(nullable=True) 
+    category : Mapped[Optional[str]] = mapped_column(nullable=True) 
+    datetime : Mapped[datetime] #= mapped_column(DateTime,server_default=func.now())
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 session = session = Session()
+
+print('sss')
+
+#add data
+#session.add(sales(age=45,id=2,client_id=4,price=3.4,quantity=33,age_range='44',category='sports'))
+print('adding')
+
+session.commit()
+print('commit !!')
+
+print(session.query(sales).all())
 
 
 
