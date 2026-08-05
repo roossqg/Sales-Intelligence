5f71e96c6ee418996b9cd65094741593d11d2a9b
Neima <a01399122@gmail.com>
2026-08-05 19:53:12 -0300

    dev 1

---
+++ extras/ml.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.markdown(" Page 3")
+st.sidebar.markdown('Page 3')
+
---
+++ extras/stats.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.markdown(" Page 2")
+st.sidebar.markdown('Page 3')
+
+st.image('/home/augusto2/Sales_inteligence_dh-api/extras/HD5y4vhboAQJt1d.jpg')
