c04bb7a19e3ae84381e1304b8702030ecad0c2ad
Neima <a01399122@gmail.com>
2026-08-16 21:31:14 -0300

    forecast

---
+++ src/Inference/forecasting.py
@@ -0,0 +1,64 @@
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.tsatools import adfuller
+import matplotlib.pyplot as plt
+import numpy as np
+from statsmodels.graphics.tsaplots import plot_acf,plot_pacf
+import pandas as pd
+
+
+#times periods + (qtd,revenue)
+#groups per category,age,products
+#conf interval + plots + evaluate-compare
+#cautions of less sales
+
+#plot eavluations for confidence
+
+def select_forecast_model(data,col,time_period,predict_range) -> graphs:
+    #plot series of respective time series
+    plt.plot(y=data[col],x=data[time_period],color='red',label='data')
+
+
+    
+    #test
+    test = adfuller(data[col]) 
+
+    #ar,ma,armax
+    plot_acf(data[col],lags=20,alpha=0.05)
+    plot_pacf(data[col],lags=20,alpha=0.05)
+
+
+    sea = seasonal_decompose(x=data[time_period],y=data[col])
+    sea.plot()
+
+
+
+def forecast_arima(time_period,predict_range,steps,col,data: pd.data,model: tuple) -> table predicts for time range:
+
+
+    #select model:
+    select_forecast_model(data,col,time_period,predict_range)
+
+    #predicts
+    if model[1] != 0:
+        model = ARIMA(x=data[time_period],y=data[col],order=model,exog=model[4]).fit()
+    else:
+        model = ARIMA(x=data[time_period],y=data[col],order=model).fit()
+
+    results_predicts = model.get_predictions(steps=steps)
+    results_forecast = model.get_forecast(steps=steps)
+
+    #evaluate
+    metrics = {'AIC': model.aic,'BIC':model.bic}
+    summary = model.summary()
+
+    plt.plot(results_forecast.index,results_forecast,color='blue',label='forecast')
+    plt.plot(results_forecast.index,results_forecast,color='blue',label='predicts')
+
+    #print box-jenkis method
+
+    plt.legend()
+    plt.show()
+
+
+    return 
